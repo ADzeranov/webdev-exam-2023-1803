fd68162aa2a19e4a524d341c7e42d1bda7cc2a3b
ADzeranov <91114849+ADzeranov@users.noreply.github.com>
2023-03-17 23:11:12 +0300

    9 step

--- person.js
+++ person.js
@@ -213,3 +213,172 @@ function changeTotalPriceForPersons(event) {            //Измение пол�
     let price = (guideServiceCost() * hoursNumber() * isThisDayOff() + isItMorningOrEvening() + numberOfVisitors() + checkOptionSecond()) * checkOptionFirst();
     form.elements["price"].value = parseInt(price);
 }
+
+function clickOnPen(event) {                            //Открытие модального окна редактирования заявки
+    if (!event.target.classList.contains("bi-pencil-square")) return;
+    let modal = document.querySelector("#showTask");
+    modal.querySelector("#exampleModalLabel").textContent = "Редактирование заявки";
+    let guideId = event.target.parentNode.parentNode.getAttribute("data-guide-id");
+    let taskId = event.target.parentNode.parentNode.id;
+    modal.querySelector(".create-btn").setAttribute("data-task-id", taskId);
+    let guideFio = modal.querySelector("#name");
+    let priceHour = document.querySelector(".table-routes");
+    nameOfGuide(guideId).then((response) => guideFio.value = response);
+
+    let routeName = modal.querySelector("#route");
+    routeName.value = event.target.parentNode.parentNode.children[1].textContent;
+
+    let date = modal.querySelector("#date");
+    date.removeAttribute("readonly");
+    let newDate = new Date();
+    newDate.setDate(newDate.getDate() + 1);
+    date.setAttribute("min", newDate.toJSON().slice(0, 10));
+    let strDate = event.target.parentNode.parentNode.children[2].textContent.split(".");
+    let trueDate = new Date(strDate[2] + "-" + strDate[1] + "-" + strDate[0]);
+    date.value = trueDate.toJSON().slice(0, 10);
+
+    let time = modal.querySelector("#time");
+    time.removeAttribute("readonly");
+    let timeRoute = event.target.parentNode.parentNode.getAttribute("data-time");
+    time.value = timeRoute;
+
+    let duration = modal.querySelector("#selectLength");
+    duration.removeAttribute("disabled");
+    let durationRoute = event.target.parentNode.parentNode.getAttribute("data-duration");
+    duration.value = durationRoute;
+
+    let personsRange = modal.querySelector("#customRange2");
+    personsRange.removeAttribute("readonly");
+    personsRange.removeAttribute("disabled");
+    personsRange.oninput = changeTotalPriceForPersons;
+    let personsText = modal.querySelector("#number-people");
+    let persons = event.target.parentNode.parentNode.getAttribute("data-persons");
+    personsRange.value = persons;
+    personsText.value = persons;
+
+    modal.querySelector(".options").innerHTML = "";
+
+    let option1 = modal.querySelector(".form-switch-option1");
+    option1.innerHTML = "";
+    let switchInput1 = document.createElement("input");
+    switchInput1.classList.add("form-check-input");
+    switchInput1.setAttribute("type", "checkbox");
+    switchInput1.setAttribute("role", "switch");
+    switchInput1.setAttribute("id", "option1");
+    switchInput1.oninput = changeTotalPrice;
+    let switchLabel1 = document.createElement("label");
+    switchLabel1.classList.add("form-check-label");
+    switchLabel1.setAttribute("for", "option1");
+    switchLabel1.textContent = "Использовать услуги быстрого гида";
+    let routeOptionF = event.target.parentNode.parentNode.getAttribute("data-option1");
+    if (routeOptionF == "true") {
+        switchInput1.checked = true;
+        switchInput1.setAttribute("readonly", "");
+        switchInput1.setAttribute("disabled", "");
+    } else {
+        switchInput1.checked = false;
+        switchInput1.removeAttribute("readonly");
+        switchInput1.removeAttribute("disabled");
+    }
+    option1.append(switchInput1);
+    option1.append(switchLabel1);
+
+    let option2 = modal.querySelector(".form-switch-option2");
+    option2.innerHTML = "";
+    let switchInput2 = document.createElement("input");
+    switchInput2.classList.add("form-check-input");
+    switchInput2.setAttribute("type", "checkbox");
+    switchInput2.setAttribute("role", "switch");
+    switchInput2.setAttribute("id", "option2");
+    switchInput2.oninput = changeTotalPrice;
+    let switchLabel2 = document.createElement("label");
+    switchLabel2.classList.add("form-check-label");
+    switchLabel2.setAttribute("for", "option2");
+    switchLabel2.textContent = "Тематические сувениры для посетителей";
+    let routeOptionS = event.target.parentNode.parentNode.getAttribute("data-option2");
+    if (routeOptionS == "true") {
+        switchInput2.checked = true;
+        switchInput2.setAttribute("readonly", "");
+        switchInput2.setAttribute("disabled", "");
+    } else {
+        switchInput2.checked = false;
+        switchInput2.removeAttribute("readonly");
+        switchInput2.removeAttribute("disabled");
+    }
+    option2.append(switchInput2);
+    option2.append(switchLabel2);
+
+    let price = document.querySelector("#price");
+    let priceRoute = event.target.parentNode.parentNode.children[3].textContent;
+    price.value = priceRoute;
+
+    modal.querySelector(".back-btn").classList.remove("d-none");
+    let createBtn = modal.querySelector(".create-btn");
+    createBtn.removeAttribute("data-bs-dismiss");
+    createBtn.textContent = "Сохранить";
+    createBtn.classList.add("create-change-task");
+}
+
+function createRoute(data, number) {                    //Создание заявки
+    let table = document.querySelector(".table-routes");
+    let row = document.createElement("tr");
+    row.setAttribute("id", data.id);
+    row.setAttribute("data-guide-id", data.guide_id);
+    row.setAttribute("data-time", data.time);
+    row.setAttribute("data-duration", data.duration);
+    row.setAttribute("data-persons", data.persons);
+    row.setAttribute("data-option1", data.optionFirst);
+    row.setAttribute("data-option2", data.optionSecond);
+
+    let th = document.createElement("th");
+    th.setAttribute("scope", "row");
+    th.textContent = number;
+    row.append(th);
+
+    let name = document.createElement("td");
+    nameOfRoute(data.route_id).then((response) => name.textContent = response);
+    row.append(name);
+
+    let dateRoute = document.createElement("td");
+    dateee = new Date(data.date);
+    DayMonthYear = dateee.toJSON().slice(0, 10).split("-");
+    dateRoute.textContent = DayMonthYear[2] + "." + DayMonthYear[1] + "." + DayMonthYear[0];
+    row.append(dateRoute);
+
+    let priceRoute = document.createElement("td");
+    priceRoute.textContent = data.price;
+    row.append(priceRoute);
+
+    let actions = document.createElement("td");
+    actions.classList.add("d-flex");
+    actions.classList.add("flex-wrap");
+    let eye = document.createElement("i");
+    eye.classList.add("bi");
+    eye.classList.add("bi-eye-fill");
+    eye.classList.add("mx-2");
+    eye.setAttribute("data-bs-toggle", "modal");
+    eye.setAttribute("data-bs-target", "#showTask");
+    eye.onclick = clickOnEye;
+    actions.append(eye);
+
+    let pen = document.createElement("i");
+    pen.classList.add("bi");
+    pen.classList.add("bi-pencil-square");
+    pen.classList.add("mx-2");
+    pen.setAttribute("data-bs-toggle", "modal");
+    pen.setAttribute("data-bs-target", "#showTask");
+    pen.onclick = clickOnPen;
+    actions.append(pen);
+
+    let trash = document.createElement("i");
+    trash.classList.add("bi");
+    trash.classList.add("bi-trash-fill");
+    trash.classList.add("ms-2");
+    trash.setAttribute("data-bs-toggle", "modal");
+    trash.setAttribute("data-bs-target", "#deleteTask");
+    trash.onclick = clickOnTrash;
+    actions.append(trash);
+    row.append(actions);
+
+    table.append(row);
+}
